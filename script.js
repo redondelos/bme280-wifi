ed9e974672cf8e872e25b2bab6d5dd8a384403b1
redondelos <carlos.puente.r@outlook.com>
2026-08-10 20:04:49 -0500

    Initial

---
+++ script.js
@@ -0,0 +1,54 @@
+console.log("Hello ESP 32!");
+
+document.getElementById("message").textContent = "JS is running!";
+
+const ESP32 = "http://192.168.1.77";
+
+function updateSensor()
+{
+    fetch(ESP32 + "/bme280")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        document.getElementById("temperature").textContent = data.temperature + " ºC";
+        document.getElementById("pressure").textContent = data.pressure + " mmHg";
+        document.getElementById("humidity").textContent = data.humidity + " %";
+
+    })
+};
+
+
+function updateLedStatus()
+{
+    fetch(ESP32 + "/led/status")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        
+        if (data.LED)
+            {
+                document.getElementById("ledStatus").textContent = "ON";
+            } 
+            else
+                {
+                    document.getElementById("ledStatus").textContent = "OFF";
+                }
+            });
+};
+
+document.getElementById("ledOn").addEventListener("click", function()
+        {
+            fetch(ESP32 + "/led/on")
+                .then(() => updateLedStatus());
+        });
+        
+document.getElementById("ledOff").addEventListener("click", function()
+        {
+            fetch(ESP32 + "/led/off")
+                .then(() => updateLedStatus());
+        });
+
+updateLedStatus();
+updateSensor();
+setInterval(updateSensor, 1000);

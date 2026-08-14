8ea43b3ce43c8c99754894e20f44f8c9022d734b
redondelos <carlos.puente.r@outlook.com>
2026-08-13 20:36:49 -0500

    Commented out LED code that's not gonna be used for now

--- script.js
+++ script.js
@@ -47,110 +47,110 @@ function updateSensor()
 };
 
 
-function updateLedStatus()
-{
-    fetch(ESP32 + "/led/status")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+// function updateLedStatus()
+// {
+//     fetch(ESP32 + "/led/status")
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
         
-        if (data.LED)
-            {
-                document.getElementById("ledStatus").textContent = "ON";
-            } 
-            else
-                {
-                    document.getElementById("ledStatus").textContent = "OFF";
-                }
-            });
-};
+//         if (data.LED)
+//             {
+//                 document.getElementById("ledStatus").textContent = "ON";
+//             } 
+//             else
+//                 {
+//                     document.getElementById("ledStatus").textContent = "OFF";
+//                 }
+//             });
+// };
 
-if (ledOn) {
-        ledOn.addEventListener("click", function()
-        {
-            fetch(ESP32 + "/led/on")
-                .then(() => updateLedStatus());
-        });
-    }
+// if (ledOn) {
+//         ledOn.addEventListener("click", function()
+//         {
+//             fetch(ESP32 + "/led/on")
+//                 .then(() => updateLedStatus());
+//         });
+//     }
        
-if (ledOff) {
-        ledOff.addEventListener("click", function()
-        {
-            fetch(ESP32 + "/led/off")
-                .then(() => updateLedStatus());
-        });
-    }
+// if (ledOff) {
+//         ledOff.addEventListener("click", function()
+//         {
+//             fetch(ESP32 + "/led/off")
+//                 .then(() => updateLedStatus());
+//         });
+//     }
 
-function updateModeStatus()
-{
-    fetch(ESP32 + "/mode/status")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+// function updateModeStatus()
+// {
+//     fetch(ESP32 + "/mode/status")
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
             
-            if (data.MODE)
-                {
-                    document.getElementById("modeStatus").textContent = "AUTO";
-                } 
-                else
-                    {
-                        document.getElementById("modeStatus").textContent = "MANUAL";
-                    }
-                });
+//             if (data.MODE)
+//                 {
+//                     document.getElementById("modeStatus").textContent = "AUTO";
+//                 } 
+//                 else
+//                     {
+//                         document.getElementById("modeStatus").textContent = "MANUAL";
+//                     }
+//                 });
 
-};
+// };
 
-if (modeAuto) {
-        modeAuto.addEventListener("click", function()
-        {
-            fetch(ESP32 + "/mode/auto")
-                .then(() => updateModeStatus());
-        });
+// if (modeAuto) {
+//         modeAuto.addEventListener("click", function()
+//         {
+//             fetch(ESP32 + "/mode/auto")
+//                 .then(() => updateModeStatus());
+//         });
 
-    }
+//     }
 
-if (modeManual) {
-        modeManual.addEventListener("click", function()
-        {
-            fetch(ESP32 + "/mode/manual")
-                .then(() => updateModeStatus());
-        });
+// if (modeManual) {
+//         modeManual.addEventListener("click", function()
+//         {
+//             fetch(ESP32 + "/mode/manual")
+//                 .then(() => updateModeStatus());
+//         });
 
-    }
+//     }
 
-function updateLedStatus_2()
-{
-    fetch(ESP32 + "/board-led/status")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+// function updateLedStatus_2()
+// {
+//     fetch(ESP32 + "/board-led/status")
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
         
-        if (data.BOARD_LED)
-            {
-                document.getElementById("ledStatus_2").textContent = "ON";
-            } 
-            else
-                {
-                    document.getElementById("ledStatus_2").textContent = "OFF";
-                }
-            });
-};
+//         if (data.BOARD_LED)
+//             {
+//                 document.getElementById("ledStatus_2").textContent = "ON";
+//             } 
+//             else
+//                 {
+//                     document.getElementById("ledStatus_2").textContent = "OFF";
+//                 }
+//             });
+// };
 
-if (ledOn_2) {
-        ledOn_2.addEventListener("click", function()
-        {
-            fetch(ESP32 + "/board-led/on")
-                .then(() => updateLedStatus_2());
-        });
-    }
+// if (ledOn_2) {
+//         ledOn_2.addEventListener("click", function()
+//         {
+//             fetch(ESP32 + "/board-led/on")
+//                 .then(() => updateLedStatus_2());
+//         });
+//     }
        
-if (ledOff_2) {
-        ledOff_2.addEventListener("click", function()
-        {
-            fetch(ESP32 + "/board-led/off")
-                .then(() => updateLedStatus_2());
-        });
-    }
+// if (ledOff_2) {
+//         ledOff_2.addEventListener("click", function()
+//         {
+//             fetch(ESP32 + "/board-led/off")
+//                 .then(() => updateLedStatus_2());
+//         });
+//     }
 
 updateLedStatus();
 updateLedStatus_2();
